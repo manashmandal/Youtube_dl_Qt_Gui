0219f68eb9e681d217cfa5317e536455cf9f3097
Manash Mandal <manashmndl@gmail.com>
2015-03-15 22:12:32 +0600

    Update workingDirectory.cpp

--- workingDirectory.cpp
+++ workingDirectory.cpp
@@ -9,9 +9,10 @@ int main(int argc, char *argv[])
     QProcess process;
 
     QString address = "C:\\Users\\Manash\\Documents\\Command";
+    QString downloadLink = "https://www.youtube.com/watch?v=xMq-FyDBeBA";
 
     process.setWorkingDirectory(address);
-    process.start("cmd.exe", QStringList() << "/cyoutube-dl --help");
+    process.start("cmd.exe", QStringList() << "/cyoutube-dl " + downloadLink);
 
     while(process.waitForReadyRead(-1)){
         qDebug() << process.readAll();
