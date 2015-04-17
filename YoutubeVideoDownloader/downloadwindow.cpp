ebf382e62afb445c26235f00c459cd0b440d52d1
Manash Mandal <manashmndl@gmail.com>
2015-04-18 04:07:15 +0600

    optimized code

--- YoutubeVideoDownloader/downloadwindow.cpp
+++ YoutubeVideoDownloader/downloadwindow.cpp
@@ -6,6 +6,15 @@ downloadWindow::downloadWindow(QWidget *parent) :
     ui(new Ui::downloadWindow)
 {
     ui->setupUi(this);
+    process = new QProcess(this);
+    //Use this when are you using debug
+    process->setWorkingDirectory(address);
+    //Use this when you are using release
+    //process->setWorkingDirectory(QDir::currentPath());
+
+    //connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(readyForOutput()));
+    //connect(this, SIGNAL(download_progress()), ui->downloadProgress, SLOT(setValue(int)));
+    //createTaskbar();
 }
 
 downloadWindow::~downloadWindow()
@@ -16,4 +25,50 @@ downloadWindow::~downloadWindow()
 void downloadWindow::on_exitButton_clicked()
 {
     this->close();
+}
+
+void downloadWindow::on_link_textEdited(const QString &arg1)
+{
+    downloadLink = arg1;
+    qDebug() << downloadLink;
+}
+
+void downloadWindow::on_downloadButton_clicked()
+{
+    previousDownloadLink = downloadLink;
+
+}
+
+void downloadWindow::readyForOutput()
+{
+    updatedStatus = process->readLine();
+    ui->status->setText(updatedStatus);
+    analyzeOutput(updatedStatus);
+}
+
+void downloadWindow::analyzeOutput(QString prog)
+{
+    QString theProgress = "123";
+   int perc_index, paren_index;
+   int progressInt;
+
+   QString *thatStatus;
+   thatStatus = &prog;
+
+    if (thatStatus->contains('%')){
+        perc_index = thatStatus->indexOf('%');
+        paren_index = thatStatus->indexOf(']');
+
+        QString percentage_progress = "";
+
+        progressInt = (int) thatStatus->mid(paren_index + 1, perc_index - paren_index - 1).remove(QChar(''), Qt::CaseInsensitive).toFloat();
+
+
+        //qDebug() << "x: " << x << "\n";
+        qDebug() << "Truncated string is: " << percentage_progress << "\n";
+        qDebug() << "String to int conversion: " << progressInt << "\n";
+
+
+        ui->downloadProgress->setValue(progressInt);
+    }
 }
--- YoutubeVideoDownloader/downloadwindow.h
+++ YoutubeVideoDownloader/downloadwindow.h
@@ -1,7 +1,11 @@
 #ifndef DOWNLOADWINDOW_H
 #define DOWNLOADWINDOW_H
-
+#include <QProcess>
+#include <QWinTaskbarButton>
+#include <QWinTaskbarProgress>
+#include <QDebug>
 #include <QMainWindow>
+#include <QShowEvent>
 
 namespace Ui {
 class downloadWindow;
@@ -12,11 +16,38 @@ class downloadWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    //Main Download Handler
+    QProcess *process;
+    QString downloadLink;
+    QString previousDownloadLink;
+    QString address = "C:\\Users\\Manash\\Documents\\QCommand";
+    QString updatedStatus;
+
+    // Showing Progress on the taskbar
+    void createTaskbar();
+    QWinTaskbarButton *taskbarButton;
+    QWinTaskbarProgress *taskbarProgress;
+    void showEvent(QShowEvent *e);
+
+
+    //Default
     explicit downloadWindow(QWidget *parent = 0);
     ~downloadWindow();
 
+
+public slots:
+    void readyForOutput();
+    void analyzeOutput(QString prog);
+
+signals:
+    int download_progress();
+
 private slots:
     void on_exitButton_clicked();
+
+    void on_link_textEdited(const QString &arg1);
+
+    void on_downloadButton_clicked();
 
 private:
     Ui::downloadWindow *ui;
