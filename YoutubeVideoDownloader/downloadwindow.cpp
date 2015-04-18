dfbbfcd216e45449730d63399826932bf364a0e2
Manash Mandal <manashmndl@gmail.com>
2015-04-18 09:43:06 +0600

    updated class file

--- YoutubeVideoDownloader/downloadwindow.cpp
+++ YoutubeVideoDownloader/downloadwindow.cpp
@@ -7,14 +7,17 @@ downloadWindow::downloadWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     process = new QProcess(this);
+    ui->pauseButton->setEnabled(false);
+    ui->stopButton->setEnabled(false);
+
     //Use this when are you using debug
     process->setWorkingDirectory(address);
     //Use this when you are using release
     //process->setWorkingDirectory(QDir::currentPath());
 
-    //connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(readyForOutput()));
-    //connect(this, SIGNAL(download_progress()), ui->downloadProgress, SLOT(setValue(int)));
-    //createTaskbar();
+    connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(readyForOutput()));
+    connect(this, SIGNAL(download_progress()), ui->downloadProgress, SLOT(setValue(int)));
+    createTaskbar();
 }
 
 downloadWindow::~downloadWindow()
@@ -35,9 +38,14 @@ void downloadWindow::on_link_textEdited(const QString &arg1)
 
 void downloadWindow::on_downloadButton_clicked()
 {
-    previousDownloadLink = downloadLink;
-
+    previousDownloadLink = downloadLink; //Kept a copy for resuming
+    //Disabling the buttons and text editors while downloading
+    ui->link->setEnabled(false);
+    ui->downloadButton->setEnabled(false);
+    ui->pauseButton->setEnabled(true);
+    ui->stopButton->setEnabled(true);
 }
+
 
 void downloadWindow::readyForOutput()
 {
@@ -46,9 +54,9 @@ void downloadWindow::readyForOutput()
     analyzeOutput(updatedStatus);
 }
 
+//Analyzing output for progress and ETA
 void downloadWindow::analyzeOutput(QString prog)
 {
-    QString theProgress = "123";
    int perc_index, paren_index;
    int progressInt;
 
@@ -59,16 +67,30 @@ void downloadWindow::analyzeOutput(QString prog)
         perc_index = thatStatus->indexOf('%');
         paren_index = thatStatus->indexOf(']');
 
-        QString percentage_progress = "";
+        progressInt = (int) thatStatus->mid(paren_index + 1, perc_index - paren_index - 1).remove(QChar(' ')).toFloat();
 
-        progressInt = (int) thatStatus->mid(paren_index + 1, perc_index - paren_index - 1).remove(QChar(''), Qt::CaseInsensitive).toFloat();
-
-
-        //qDebug() << "x: " << x << "\n";
-        qDebug() << "Truncated string is: " << percentage_progress << "\n";
         qDebug() << "String to int conversion: " << progressInt << "\n";
-
 
         ui->downloadProgress->setValue(progressInt);
     }
+}
+
+//Creating Taskbar
+void downloadWindow::createTaskbar()
+{
+    taskbarButton = new QWinTaskbarButton(this);
+    taskbarButton->setWindow(windowHandle());
+    taskbarProgress = taskbarButton->progress();
+    taskbarProgress->show();
+    connect(ui->downloadProgress, SIGNAL(valueChanged(int)), taskbarProgress, SLOT(setValue(int)));
+
+}
+
+//Taskbar Progress Function
+void downloadWindow::showEvent(QShowEvent *e)
+{
+#ifdef Q_OS_WIN32
+    taskbarButton->setWindow(windowHandle());
+#endif
+    e->accept();
 }
